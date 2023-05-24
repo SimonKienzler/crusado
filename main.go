8aa95556f34e70aa74d66abcc719d0053fc0c53e
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2023-05-24 15:18:05 +0200

    Get current iteration from API instead of env var

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/webapi"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/work"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/workitemtracking"
 )
 
@@ -14,20 +16,17 @@ type CrusadoConfig struct {
 	OrganizationUrl     string
 	PersonalAccessToken string
 	ProjectName         string
-	CurrentIteration    string
 }
 
 func getConfig() CrusadoConfig {
 	organizationUrl := os.Getenv("AZURE_ORG_URL")
 	personalAccessToken := os.Getenv("AZURE_PAT")
 	projectName := os.Getenv("AZURE_PROJECT_NAME")
-	currentIteration := os.Getenv("CURRENT_ITERATION")
 
 	return CrusadoConfig{
 		OrganizationUrl:     organizationUrl,
 		PersonalAccessToken: personalAccessToken,
 		ProjectName:         projectName,
-		CurrentIteration:    currentIteration,
 	}
 }
 
@@ -39,32 +38,70 @@ func main() {
 
 	ctx := context.Background()
 
+	// get current iteration
+
+	workClient, err := work.NewClient(ctx, connection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentIteration, err := getCurrentIteration(ctx, workClient, config.ProjectName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v", *currentIteration.Path)
+
+	// create user story in current iteration
+
 	workitemClient, err := workitemtracking.NewClient(ctx, connection)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	project := config.ProjectName
+	workItem, err := createUserStory(ctx, workitemClient, config.ProjectName, *currentIteration.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%+v", *workItem)
+}
+
+func getCurrentIteration(ctx context.Context, workClient work.Client, projectName string) (*work.TeamSettingsIteration, error) {
+	iterations, err := workClient.GetTeamIterations(ctx, work.GetTeamIterationsArgs{
+		Project:   &projectName,
+		Timeframe: stringPointer("current"),
+	})
+	if err != nil || iterations == nil {
+		return nil, err
+	}
+
+	if len(*iterations) != 1 {
+		return nil, fmt.Errorf("Search for current iteration returned %d results", len(*iterations))
+	}
+
+	currentIteration := (*iterations)[0]
+
+	return &currentIteration, nil
+}
+
+func createUserStory(ctx context.Context, workitemClient workitemtracking.Client, projectName, currentIteration string) (*workitemtracking.WorkItem, error) {
+	project := projectName
 	workItemType := "User Story"
 	validateOnly := true
 	document := buildJSONPatchDocument(
 		"work item creation test",
 		"hello from the crusado CLI",
 		project,
-		config.CurrentIteration,
+		currentIteration,
 	)
 
-	workItem, err := workitemClient.CreateWorkItem(ctx, workitemtracking.CreateWorkItemArgs{
+	return workitemClient.CreateWorkItem(ctx, workitemtracking.CreateWorkItemArgs{
 		Document:     &document,
 		Project:      &project,
 		Type:         &workItemType,
 		ValidateOnly: &validateOnly,
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("%+v", workItem)
 }
 
 func buildJSONPatchDocument(title, description, area, iteration string) []webapi.JsonPatchOperation {
