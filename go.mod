585d3e5cb63f81082c73afb16788d8d1d9f1dab3
Simon Kienzler <SimonKienzler@users.noreply.github.com>
2023-05-25 11:47:32 +0200

    Convert to Cobra CLI

--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.20
 
 require (
 	github.com/microsoft/azure-devops-go-api/azuredevops/v7 v7.1.0
+	github.com/spf13/cobra v1.7.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require github.com/google/uuid v1.1.1 // indirect
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,90 +1,9 @@
 package main
 
 import (
-	"context"
-	"io/ioutil"
-	"log"
-
-	"github.com/simonkienzler/crusado/pkg/config"
-	"github.com/simonkienzler/crusado/pkg/userstorytemplates"
-	"github.com/simonkienzler/crusado/pkg/workitems"
-
-	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
-	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/work"
-	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/workitemtracking"
-	yaml "gopkg.in/yaml.v3"
+	"github.com/simonkienzler/crusado/cmd"
 )
 
 func main() {
-	ctx := context.Background()
-	useDryRunMode := true
-
-	// create user story template struct
-	userStoryTemplateExample, err := ioutil.ReadFile("./example/userStoryTemplate.yaml")
-	if err != nil {
-		log.Fatalf("Could not read example template file: %s", err)
-	}
-
-	userStoryTemplate := config.UserStoryTemplate{}
-	yaml.Unmarshal(userStoryTemplateExample, &userStoryTemplate)
-
-	workitemsService, err := createWorkitemsService(ctx, useDryRunMode)
-	if err != nil {
-		log.Fatalf("Error during service creation: %s", err)
-	}
-
-	userStoryTemplatesService := &userstorytemplates.Service{
-		WorkitemsService: *workitemsService,
-	}
-
-	if err := userStoryTemplatesService.CreateWorkitemsFromUserStoryTemplate(ctx, userStoryTemplate); err != nil {
-		log.Fatalf("Error during user story template creation: %s", err)
-	}
-}
-
-func createWorkitemsService(ctx context.Context, useDryRunMode bool) (*workitems.Service, error) {
-	crusadoConfig := config.GetConfig(true)
-
-	// create a connection to the organization
-	connection := azuredevops.NewPatConnection(crusadoConfig.OrganizationUrl, crusadoConfig.PersonalAccessToken)
-
-	// create clients required by the workitems service
-	workitemClient, err := workitemtracking.NewClient(ctx, connection)
-	if err != nil {
-		return nil, err
-	}
-
-	workClient, err := work.NewClient(ctx, connection)
-	if err != nil {
-		return nil, err
-	}
-
-	// configure the workitems service
-	workitemsService := workitems.Service{
-		WorkitemClient: workitemClient,
-		WorkClient:     workClient,
-		DryRun:         useDryRunMode,
-		ProjectConfig: config.ProjectConfig{
-			Name:     crusadoConfig.ProjectName,
-			AreaPath: crusadoConfig.ProjectName,
-		},
-	}
-
-	// get current iteration, either from env var or from the API
-	workitemsService.ProjectConfig.IterationPath = crusadoConfig.IterationPath
-	if !crusadoConfig.UseIterationPathFromEnvVar {
-		log.Print("Getting path of current iteration...")
-
-		currentIteration, err := workitemsService.GetCurrentIteration(ctx)
-		if err != nil {
-			return nil, err
-		}
-
-		log.Printf("Got current iteration path: %+v", *currentIteration.Path)
-		workitemsService.ProjectConfig.IterationPath = *currentIteration.Path
-	} else {
-		log.Printf("Using configured iteration path: %+v", workitemsService.ProjectConfig.IterationPath)
-	}
-
-	return &workitemsService, nil
+	cmd.Execute()
 }
